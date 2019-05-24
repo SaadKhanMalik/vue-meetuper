d7a3920a239d9923b85c5dbdee786665040dbc1c
Saad Khan Malik <social@saadkhanmalik.com>
2019-05-24 06:04:37 +0300

    Initial Router Setup

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="section">
+        <div>
+            <h1 class="title">Categories</h1>
+            <div class="columns cover is-multiline is-mobile">
+            <div 
+                class="column is-one-quarter"
+                :style="{'min-height': '160px'}"
+                v-for="category in categories"
+                :key="category._id"
+                >
+                <a href="#">
+                    <span class="is-primary is-top is-medium tooltip">
+                        <figure class="image is-4by3 imageFade">
+                            <img 
+                                class="is-rounded" 
+                                :src="category.image" 
+                                alt=""
+                            />
+                        </figure>
+                        <div class="subtitle m-t-xs bold">
+                            {{ category.name | capitalize }}
+                        </div>
+                    </span>
+                </a>
+            </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'CategoryItem',
+    props: {
+        categories: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+  .is-rounded {
+    border-radius: 10px !important;
+  }
+</style>
---
+++ src/pages/PageMeetupDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>PageMeetupDetail</h1>
+</template>
+
+<script>
+export default {
+  name: "PageMeetupDetail",
+  created() {
+    const meetupId = this.$route.params.id;
+  }
+};
+</script>
+
+<style scoped>
+</style>
